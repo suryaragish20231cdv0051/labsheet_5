634dfddde6d1d045f668bd67c9f957482ec5b9a6
suryaragish20231cdv0051 <ragishsurya9@gmail.com>
2026-05-06 04:35:13 +0000

    c

---
+++ calc.java
@@ -0,0 +1,7 @@
+public class calc {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        System.out.println("The sum of " + a + " and " + b + " is: " + (a + b));
+    }
+}
